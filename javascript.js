2ea8ac768d16b1b6e27009f42b5b66842824a4a2
Akash Raj R <81699585+ak18akashrajr@users.noreply.github.com>
2021-09-05 18:51:22 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,18 @@
+let inputEl=document.getElementById("input-el");
+let outputEl=document.getElementById("output-el")
+
+
+function check() {
+    let splitString = inputEl.value.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    if (inputEl.value==joinArray) {
+        outputEl.textContent=joinArray+" is palindrome";
+        
+        
+    } else {
+        outputEl.textContent=joinArray+" is not palindrome";
+        
+        
+    }
+}
